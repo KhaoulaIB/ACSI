54d7565f0286419866d220740275e5e0d6b5e356
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-03-06 18:20:07 +0100

    Update getmeminfo2.sh

--- practica2/files&Scripts/Memory/getmeminfo2.sh
+++ practica2/files&Scripts/Memory/getmeminfo2.sh
@@ -13,33 +13,44 @@ import pandas as pd
 def main():
     Memtotal = 10946740
     timestamp = []
- 
     mem = []
     memused = []
     mempercent = []
     with open('vtp.txt', 'r') as fichero:
-        linea = fichero.readline()
-        linea = fichero.readline()
-        linea = fichero.readline()
-        while (linea != ""):
-            mem.append(int(linea))
-            memused.append(int(total-int(linea)))
-            mempercent.append(float(((total-int(linea))*100)/total))
-            timestamp.append(time)
-            time += seconds
-            linea = fichero.readline()
+        # Eliminar comas y guardar el contenido en un nuevo archivo temporal
+        with open('vtp_temp.txt', 'w') as temp_file:
+            for linea in fichero:
+                linea_sin_comas = linea.replace(',', '')  # Elimina las comas
+                temp_file.write(linea_sin_comas)
 
-    data = {'Timestamp': timestamp,
-            'Capacidad disponible': mem, 'Capacidad utilizada': memused, '% Memoria utilizada': mempercent}
+    # Continuar con la lectura del archivo temporal sin comas
+    with open('vtp_temp.txt', 'r') as fichero:
+        for linea in fichero:
+            parts = linea.strip().split(' ')
+            timestamp.append(parts[0])  # Guarda el timestamp
+            free_memory = int(parts[1])  # Guarda la memoria libre
+            mem.append(free_memory)
+            memused.append(Memtotal - free_memory)  # Calcula la memoria utilizada
+            mempercent.append(((Memtotal - free_memory) * 100) / Memtotal)  # Calcula el porcentaje de memoria utilizada
 
-    df = pd.DataFrame(
-        data, columns=['Timestamp', 'Capacidad disponible', 'Capacidad utilizada', '% Memoria utilizada'])
+    # Ajuste de la columna Capacidad utilizada
+    memused_adjusted = [mem_used - 100 for mem_used in memused]
+
+    data = {
+        'Timestamp': timestamp,
+        'Capacidad disponible': mem,
+        'Capacidad utilizada': memused_adjusted,  # Utiliza la memoria ajustada
+        '% Memoria utilizada': mempercent
+    }
+
+    df = pd.DataFrame(data, columns=['Timestamp', 'Capacidad disponible', 'Capacidad utilizada', '% Memoria utilizada'])
 
     df.to_csv('monitorizacionMEM.csv', sep=';', index=False)
 
 
 if __name__ == '__main__':
     main()
+
 
 EOF
 
