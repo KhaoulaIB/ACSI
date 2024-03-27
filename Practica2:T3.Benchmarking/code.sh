59ded2f549b97f8dc12d6a412b688dc7b14180a8
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-03-27 23:30:29 +0100

    Update code.sh

--- Practica2:T3.Benchmarking/code.sh
+++ Practica2:T3.Benchmarking/code.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 for i in {1..4000}; do
-	#top -b -n 1 | grep -i# -E "Cpu\(s\)|MiB Mem" 
-top -b -n 1 | grep -i  "Cpu(s)" >> CPU.txt & vmstat -n 1 1 | grep -v -e "--io--" -e "free" | awk '{print $4}' >> vmstat.txt
- 
+	top -b -n 1 | grep -i# -E "Cpu\(s\)|MiB Mem"  
     sleep 2
 done
