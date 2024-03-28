fb5fb4d5dfb2ddd82e7b1d515650b0e4cf7c3e72
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-03-28 23:36:25 +0100

    Update cargaCode.sh

--- Practica2:T3.Benchmarking/scripts&stuff/cargaCode.sh
+++ Practica2:T3.Benchmarking/scripts&stuff/cargaCode.sh
@@ -2,21 +2,19 @@
 	
 
 # Definir el arreglo carga
-#carga=(6 5 4 3 2 1)
+carga=( 30000 60000 90000 120000)
 
 #carga= 30000 
 # Bucle para ejecutar sysbench cpu con diferentes cargas 
-	#for carga_valor in "${carga[@]}"; do
-	for i in {1..10}; do
-       sysbench cpu --cpu-max-prime=200000 --threads=2 --time=0 --events=200000 run >> CARGAthreads2.txt
-       
+	for carga_valor in "${carga[@]}"; do
+	for i in {1..15}; do
+       sysbench cpu --cpu-max-prime="$carga_valor" --threads=2 --time=0 --events="$carga_valor" run >> CARGA.txt
        
         sleep 2
     done
-   #echo "Ejecución $i threads completa" >> CPU.txt
-  #   echo "Ejecución $i threads completa" >> vmstat.txt
+   echo "Ejecución $i completa" >> CPU.txt
     
-#done
+done
 	pkill -f code.sh
 #acabar la monitorizacion
   # Detener el script code.sh que ejecuta top
